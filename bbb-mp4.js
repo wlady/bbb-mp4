b9e2ae97ebed980a8efd51675412e8f2b847f151
Vladimir Zabara <wlady2001@gmail.com>
2022-02-03 13:40:27 +0200

    screen size is 1920x1080

--- bbb-mp4.js
+++ bbb-mp4.js
@@ -7,7 +7,7 @@ var disp_num = Math.floor(Math.random() * (200 - 99) + 99);
 var xvfb = new Xvfb({
     displayNum: disp_num,
     silent: true,
-    xvfb_args: ["-screen", "0", "1280x800x24", "-ac", "-nolisten", "tcp", "-dpi", "96", "+extension", "RANDR"]
+    xvfb_args: ["-screen", "0", "1920x1080x24", "-ac", "-nolisten", "tcp", "-dpi", "96", "+extension", "RANDR"]
 });
 var width = 1280;
 var height = 800;
