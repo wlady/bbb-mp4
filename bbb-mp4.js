0f0c6ac47435178ecaddd9487396f2eb5ef62c77
Vladimir Zabara <wlady2001@gmail.com>
2022-02-04 19:46:27 +0200

    remove odd chrome options

--- bbb-mp4.js
+++ bbb-mp4.js
@@ -7,10 +7,10 @@ var disp_num = Math.floor(Math.random() * (200 - 99) + 99);
 var xvfb = new Xvfb({
     displayNum: disp_num,
     silent: true,
-    xvfb_args: ["-screen", "0", "1940x110x24", "-ac", "-nolisten", "tcp", "-dpi", "96", "+extension", "RANDR"]
+    xvfb_args: ["-screen", "0", "1930x1090x24", "-ac", "-nolisten", "tcp", "-dpi", "96", "+extension", "RANDR"]
 });
-var width = 1940;
-var height = 1100;
+var width = 1930;
+var height = 1090;
 var options = {
     headless: false,
     args: [
